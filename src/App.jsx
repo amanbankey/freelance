d8c4e643612ae074d4b8df8c8e4264c32bd37699
amanbankey <amanbankey8@gmail.com>
2026-03-23 01:13:34 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Services from './pages/Services'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +17,15 @@ function App() {
       <div className='bg-red-300 border border-2'> 
            aman
       </div>
+      <Routes  > 
+       
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+
+
     </div>
   )
 }
